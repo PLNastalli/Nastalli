5a9667eba00e5ddadb87ac0a506786442294a665
PLNastalli <103285104+PLNastalli@users.noreply.github.com>
2026-09-07 14:39:51 -0300

    feat: add ring 3 GDT selectors

--- crates/arch/src/gdt.rs
+++ crates/arch/src/gdt.rs
@@ -4,32 +4,50 @@ use x86_64::structures::gdt::{Descriptor, GlobalDescriptorTable, SegmentSelector
 use x86_64::structures::tss::TaskStateSegment;
 
 const DOUBLE_FAULT_IST_INDEX: u16 = 0;
+const KERNEL_INTERRUPT_STACK_SIZE: usize = 16 * 1024;
 
 lazy_static! {
     static ref TSS: TaskStateSegment = {
         let mut tss = TaskStateSegment::new();
-        static STACK: [u8; 4096] = [0; 4096];
-        let stack_start = VirtAddr::from_ptr(&STACK);
-        let stack_end = stack_start + STACK.len();
-        tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = stack_end;
+
+        static DOUBLE_FAULT_STACK: [u8; 4096] = [0; 4096];
+        let double_fault_stack_start = VirtAddr::from_ptr(&DOUBLE_FAULT_STACK);
+        let double_fault_stack_end = double_fault_stack_start + DOUBLE_FAULT_STACK.len();
+        tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = double_fault_stack_end;
+
+        static KERNEL_INTERRUPT_STACK: [u8; KERNEL_INTERRUPT_STACK_SIZE] =
+            [0; KERNEL_INTERRUPT_STACK_SIZE];
+        let kernel_stack_start = VirtAddr::from_ptr(&KERNEL_INTERRUPT_STACK);
+        let kernel_stack_end = kernel_stack_start + KERNEL_INTERRUPT_STACK.len();
+        tss.privilege_stack_table[0] = kernel_stack_end;
+
         tss
     };
     static ref GDT: (GlobalDescriptorTable, Selectors) = {
         let mut gdt = GlobalDescriptorTable::new();
         let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
         let data_selector = gdt.add_entry(Descriptor::kernel_data_segment());
+        let user_data_selector = gdt.add_entry(Descriptor::user_data_segment());
+        let user_code_selector = gdt.add_entry(Descriptor::user_code_segment());
         let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
-        (gdt, Selectors {
-            code_selector,
-            data_selector,
-            tss_selector,
-        })
+        (
+            gdt,
+            Selectors {
+                code_selector,
+                data_selector,
+                user_data_selector,
+                user_code_selector,
+                tss_selector,
+            },
+        )
     };
 }
 
 struct Selectors {
     code_selector: SegmentSelector,
     data_selector: SegmentSelector,
+    user_data_selector: SegmentSelector,
+    user_code_selector: SegmentSelector,
     tss_selector: SegmentSelector,
 }
 
@@ -47,6 +65,10 @@ pub fn init() {
         ES::set_reg(GDT.1.data_selector);
         load_tss(GDT.1.tss_selector);
     }
+}
+
+pub fn user_selectors() -> (SegmentSelector, SegmentSelector) {
+    (GDT.1.user_code_selector, GDT.1.user_data_selector)
 }
 
 #[cfg(test)]
